f447a603ae6687403246fc33900dd4d3659045d0
arunav7 <harsharunav9@gmail.com>
2022-12-09 21:27:55 +0530

    merge conflict starting point

--- Demo.java
+++ Demo.java
@@ -2,5 +2,7 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("sJust a demo text");
         System.out.println("New Commit");
+
+        System.out.println("starting point of merge conflict in master");
     }
 }

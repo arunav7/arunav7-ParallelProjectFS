bea3523344c226ff6e8359cb7e831a3c38369811
arunav7 <harsharunav9@gmail.com>
2022-12-10 03:20:51 +0530

    updated Demo.java file

--- Demo.java
+++ Demo.java
@@ -4,5 +4,11 @@ public class Demo {
         System.out.println("New Commit");
 
         System.out.println("New Commit on drop1 merge conflict");
+
+        int a = 2;
+        int b = 3;
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
     }
 }

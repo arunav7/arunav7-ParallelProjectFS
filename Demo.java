000d026892128a7219b91480ffbb7a4feeda5112
arunav7 <harsharunav9@gmail.com>
2022-12-09 21:28:59 +0530

    new commit on drop1 for merge conflict

--- Demo.java
+++ Demo.java
@@ -2,5 +2,7 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("sJust a demo text");
         System.out.println("New Commit");
+
+        System.out.println("New Commit on drop1 merge conflict");
     }
 }

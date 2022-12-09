e2f668923239d86516b960affc8fd789c7ad45ec
arunav7 <harsharunav9@gmail.com>
2022-12-10 03:30:54 +0530

    updated Demo.java file (#3)

--- Demo.java
+++ Demo.java
@@ -3,8 +3,14 @@ public class Demo {
         System.out.println("sJust a demo text");
         System.out.println("New Commit");
 
+        System.out.println("starting point of merge conflict in master");
         System.out.println("New Commit on drop1 merge conflict");
 
-        System.out.println("starting point of merge conflict in master");
+        int a = 2;
+        int b = 3;
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+
     }
 }
